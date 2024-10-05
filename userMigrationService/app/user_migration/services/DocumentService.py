658bda5ec037ee5027c23e28934fb04197033a52
Juanda0 <juanda20202@hotmail.com>
2024-10-05 12:08:12 -0500

    Last version with last tears

--- userMigrationService/app/user_migration/services/DocumentService.py
+++ userMigrationService/app/user_migration/services/DocumentService.py
@@ -2,16 +2,22 @@ import requests
 import logging
 logging.basicConfig(level=logging.INFO)
 
+BASE_DOCUMENT_URL = ""
+
 
 class DocumentService:
 
     @staticmethod
     def save_user_documents(user_id, documents):
-        logging.info(f"SAVE USER DOCUMENTS REQUEST: {user_id} {documents}")
-        #ADD PROPER REQUEST response = requests.post(operator_confirmation_url, json=user_id_json)
-        logging.info(f"CIELOS; RECIBI DOCUMENTOS para user id {user_id}")
-        return
+        documents_formatted = {
+            "documents": [
+                {doc_key: urls[0]}
+                for doc_key, urls in documents.items()
+            ]
+        }
+        user_data = {"documents": documents_formatted}
+        logging.info(f"SAVE USER DOCUMENTS REQUEST: {user_id} {user_data}")
+        response = requests.post(f"{BASE_DOCUMENT_URL}/v1/users/{user_id}/transfer-docs", json=user_data)
         logging.info(f"Finished saving documents for user {user_id} with status code {response.status_code}")
-        logging.info(f"SAVE USER DOCUMENTS RESPONSE: {response.json()}")
         if not response.ok:
             raise Exception(f"Failed request for saving documents for user {user_id}. Reason {response.json()}")
--- userMigrationService/app/user_migration/services/UserService.py
+++ userMigrationService/app/user_migration/services/UserService.py
@@ -1,25 +1,47 @@
+import requests
+
 from app.user_migration.exceptions.invalid_request_exception import InvalidRequestException
 import logging
 logging.basicConfig(level=logging.INFO)
+
+BASE_USERS_URL = "https://a8ed037daf2624a7789a8d3f77bb0876-1513300967.us-east-1.elb.amazonaws.com:3002/v1/users/"
 
 
 class UserService:
 
     @staticmethod
     def delete_user_metadata(user_id):
-        print(f"CIELOS; FUI A BORRAR EL USUARIO {user_id}")
+        logging.info(f"DELETE USER: {user_id}")
+        response = requests.get(f"{BASE_USERS_URL}{user_id}")
+        data = response.json()
+        logging.info(f"DELETE USER RESPONSE: {data}")
+        if not response.ok:
+            raise Exception(f"Failed request of validation for user {user_id}. Reason {data}")
 
     @staticmethod
     def validate_transfer_solicitude(user_id):
-        print(f"CIELOS; FUI A VALIDAR QUE EL USUARIO {user_id} SOLICITARA TRANSFERENCIA")
-        response = {}
+        logging.info(f"CONFIRM USER TRANSFER SOLICITUDE: {user_id}")
+        response = requests.get(f"{BASE_USERS_URL}{user_id}/transfer")
         data = response.json()
+        logging.info(f"CONFIRM USER TRANSFER SOLICITUDE: {data}")
         if not response.ok:
-            raise Exception(f"Failed request of validation for user {user_id}. Reason {response.json()}")
+            raise Exception(f"Failed request of validation for user {user_id}. Reason {data}")
         elif not data['transferConfirmation']:
-            raise InvalidRequestException("User didnt made a transfer solicitude")
+            raise InvalidRequestException("User did not make a transfer solicitude")
         return True
 
     @staticmethod
     def create_user(user_id, user_name, user_email):
-        print(f"CIELOS; FUI A CREAR EL USUARIO {user_id}")
+        user_data ={
+            "name": user_name,
+            "email": user_email,
+            "age": 20,
+            "identification": user_id,
+            "govCarpertaId": user_id
+        }
+
+        logging.info(f"CREATE USER  REQUEST: {user_data}")
+        response = requests.post(BASE_USERS_URL, json=user_data)
+        logging.info(f"FINISHED CREATION FOR USER {user_id} with status code: {response.status_code}")
+        if not response.ok:
+            raise Exception(f"Failed request of confirmation for user {user_id}.")
