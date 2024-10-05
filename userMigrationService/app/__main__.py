727e26f6529d03beac67047955793d5ab2e8f2a3
Juanda0 <juanda20202@hotmail.com>
2024-10-05 04:11:09 -0500

    Fix config maps and add logger hook

--- userMigrationService/app/__main__.py
+++ userMigrationService/app/__main__.py
@@ -1,7 +1,24 @@
 from app import create_app
+import logging
+import sys
+
 
 app = create_app()
 
+# Configura el logger
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+# Función personalizada para manejar excepciones no capturadas
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logging.error("Ocurrió un error no esperado:", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+# Redirige las excepciones no capturadas a handle_exception
+sys.excepthook = handle_exception
 
 if __name__ == "__main__":  # Only in dev
     app.run(host="0.0.0.0", port=8080, debug=True)  # nosec
