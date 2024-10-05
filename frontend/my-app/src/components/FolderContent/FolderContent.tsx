ac7ab3d7f553d6394ffbe0ebbc599eee7eb4ccba
Anderson Olarte Martinez <anderson.olarte@lafargeholcim.com>
2024-10-05 04:31:03 -0500

    interfaces

--- frontend/my-app/src/components/FolderContent/FolderContent.tsx
+++ frontend/my-app/src/components/FolderContent/FolderContent.tsx
@@ -1,24 +1,25 @@
 import React, { useEffect } from 'react';
 import { IDisplayFolders } from '../../helpers/interfaces'; // Asegúrate de importar las interfaces correctas
+import { FILE_MICROSERVICE_URL } from '../../url';
 
 const FolderContent = (props: IDisplayFolders) => {
 
     useEffect(() => {
         const OnFolderClick = async (folderId: number) => {
             try {
-                const url = `http://localhost:3001/v1/users/${props.userId}/folders/${props.folderId}/content`
+                const url = `${FILE_MICROSERVICE_URL}/v1/users/${props.userId}/folders/${props.folderId}/content`
                 console.log(url);
                 const response = await fetch(url);
                 const responseJson = await response.json();
                 console.log(responseJson);
-                props.setFolderContent(responseJson.folder);  // Actualiza el contenido de la carpeta con la respuesta
+                props.setFolderContent(responseJson.folderContent);  // Actualiza el contenido de la carpeta con la respuesta
             } catch (error) {
                 console.error('Error fetching folder content:', error);
             }
         };
 
         OnFolderClick(props.folderId || 1);
-    }, [props.folderId])
+    }, [props.folderId, props.setFolderId])
 
     return (
         <section className='folder-container'>
