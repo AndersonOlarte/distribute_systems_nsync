9dcde2ee4489a7dbe58552df6ae8e2f1fd493d99
Juanda0 <juanda20202@hotmail.com>
2024-10-05 04:12:38 -0500

    Remove comments

--- userMigrationService/app/__main__.py
+++ userMigrationService/app/__main__.py
@@ -5,11 +5,9 @@ import sys
 
 app = create_app()
 
-# Configura el logger
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-# Función personalizada para manejar excepciones no capturadas
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
@@ -17,7 +15,6 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     logging.error("Ocurrió un error no esperado:", exc_info=(exc_type, exc_value, exc_traceback))
 
 
-# Redirige las excepciones no capturadas a handle_exception
 sys.excepthook = handle_exception
 
 if __name__ == "__main__":  # Only in dev
