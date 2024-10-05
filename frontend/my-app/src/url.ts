f8a722fe9b985d1699dd819d7628e151c752abc0
Anderson Olarte Martinez <anderson.olarte@lafargeholcim.com>
2024-10-05 07:34:12 -0500

    e

--- frontend/my-app/src/url.ts
+++ frontend/my-app/src/url.ts
@@ -1,4 +1,4 @@
 export const DOCUMENT_MICROSERVICE_URL = 'a1d7b2052bfbf4f0a9091668e9a10ec4-410648471.us-east-1.elb.amazonaws.com';
-export const FILE_MICROSERVICE_URL = 'http://localhost:3001';
+export const FILE_MICROSERVICE_URL = 'http://a44de7005d5554e58a66722933ba64fa-628257992.us-east-1.elb.amazonaws.com:3001';
 export const GOV_CARPETA_URL = 'http://govcarpeta-apis-83e1c996379d.herokuapp.com';
 export const USER_MICROSERVICE_URL = 'http://localhost:3002';
